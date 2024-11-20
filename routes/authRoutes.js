0a16c0e220392d6b0d504c8ac1637aff6a2d81a0
Ruzbihan <ruzbihanmzm@gmail.com>
2024-11-20 23:12:09 +0530

    test endpoint for deployment

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,15 +2,15 @@ const express = require("express");
 const router = express.Router();
 
 router.post("/signup", (req, res) => {
-  res.send("Signup endpoint");
+  res.send("Signup endpoint called");
 });
 
 router.post("/login", (req, res) => {
-  res.send("Login endpoint");
+  res.send("Login endpoint called");
 });
 
 router.post("/logout", (req, res) => {
-  res.send("Logout endpoint");
+  res.send("Logout endpoint called");
 });
 
 module.exports = router;
