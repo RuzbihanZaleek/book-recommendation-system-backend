cc99722c124b870f8f5d74096b11aedc8734ce32
Ruzbihan <ruzbihanmzm@gmail.com>
2024-11-30 14:09:42 +0530

    index

--- index.js
+++ index.js
@@ -4,23 +4,29 @@ const cors = require("cors");
 const { port } = require("./config/config");
 const sequelize = require("./config/database");
 const { DATABASE, SERVER } = require("./constants/constants");
+const authRoutes = require("./routes/authRoutes");
+const bookRoutes = require("./routes/bookRoutes");
+const reviewRoutes = require("./routes/reviewRoutes");
+const userBooksRoutes = require("./routes/userBooksRoutes");
+const { startDiscordBot } = require("./config/discord");
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.use("/api/auth", require("./routes/authRoutes"));
-app.use("/api/books", require("./routes/bookRoutes"));
-app.use("/api/reviews", require("./routes/reviewRoutes"));
+app.use("/api/auth", authRoutes);
+app.use("/api/books", bookRoutes);
+app.use("/api/reviews", reviewRoutes);
+app.use("/api/userbooks", userBooksRoutes);
 
-app.use((err, req, res) => {
+app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send({ message: SERVER.SERVER_ERROR });
 });
 
 sequelize
-  .sync()
+  .sync({ alter: true })
   .then(() => {
     console.log(DATABASE.MESSAGES.SYNC_SUCCESS);
   })
@@ -31,3 +37,6 @@ sequelize
 app.listen(port, () => {
   console.log(`Server running on ${port}`);
 });
+
+// Start Discord Bot
+startDiscordBot();
